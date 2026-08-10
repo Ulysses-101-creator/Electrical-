8f353be77e17c54181e2ecb1ae069a9e38aded6b
Ulysses-101-creator <propertytracker7@gmail.com>
2026-08-10 08:35:23 +0200

    fix: remove duplicate Outlet import in AppShell

--- frontend/src/components/layout/AppShell.tsx
+++ frontend/src/components/layout/AppShell.tsx
@@ -1,7 +1,5 @@
 import { Outlet } from "react-router-dom";
 
-import { Outlet } from "react-router-dom";
-
 import { BottomNav } from "@/components/layout/BottomNav";
 import { Sidebar } from "@/components/layout/Sidebar";
 
